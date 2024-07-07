711068eb8915db4869cb22f7398372d867d93ed3
Oriol Angelet <uriaeri@gmail.com>
2024-07-07 12:06:43 +0200

    Lesson 2, pt 2 - Download image with Combine

--- ConcurrencyBootcamp/ConcurrencyBootcamp/DownloadImageAsync.swift
+++ ConcurrencyBootcamp/ConcurrencyBootcamp/DownloadImageAsync.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 class DownloadImageAsyncImageLoader {
     
@@ -30,20 +31,42 @@ class DownloadImageAsyncImageLoader {
         }
         .resume()
     }
+    
+    func downloadWithCombine() -> AnyPublisher<UIImage?, Error> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(handleResponse)
+            .mapError({ $0 })   // maps the URLError from handleResponse to Error
+            .eraseToAnyPublisher()
+    }
 }
 
 class DownloadImageAsyncViewModel: ObservableObject {
     
     @Published var image: UIImage? = nil
+    
     let loader = DownloadImageAsyncImageLoader()
     
-    func fetchImage() {
+    var cancellables = Set<AnyCancellable>()
+    
+    func fetchImageCompletionHandler() {
         loader.downloadWithEscaping { [weak self] returnedImage, error in
             guard let self = self else { return }
             DispatchQueue.main.async {
                 self.image = returnedImage
             }
         }
+    }
+    
+    func fetchImageCombine() {
+        loader.downloadWithCombine()
+            .receive(on: DispatchQueue.main)
+            .sink { _ in
+                
+            } receiveValue: { [weak self] returnedImage in
+                guard let self = self else { return }
+                self.image = returnedImage
+            }
+            .store(in: &cancellables)
     }
 }
 
@@ -61,7 +84,8 @@ struct DownloadImageAsync: View {
             }
         }
         .onAppear {
-            viewModel.fetchImage()
+            // viewModel.fetchImageCompletionHandler()
+            viewModel.fetchImageCombine()
         }
     }
 }
