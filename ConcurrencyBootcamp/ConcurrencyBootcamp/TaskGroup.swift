4e073befb0dfe24d4af55c86134fde91f3da4ac0
Oriol Angelet <uriaeri@gmail.com>
2024-07-13 12:38:05 +0200

    L6, pt 2 - Fetch of 4 images with async let in DataManager

--- ConcurrencyBootcamp/ConcurrencyBootcamp/TaskGroup.swift
+++ ConcurrencyBootcamp/ConcurrencyBootcamp/TaskGroup.swift
@@ -7,14 +7,51 @@
 
 import SwiftUI
 
-final class TaskgroupViewModel: ObservableObject {
+final class TaskGroupDataManager {
+    
+    func fetchImageWithyAsyncLet() async throws -> [UIImage] {
+        async let fetchImage1 = fetchImage(urlString: "https://picsum.photos/300")
+        async let fetchImage2 = fetchImage(urlString: "https://picsum.photos/300")
+        async let fetchImage3 = fetchImage(urlString: "https://picsum.photos/300")
+        async let fetchImage4 = fetchImage(urlString: "https://picsum.photos/300")
+        
+        let (image1, image2, image3, image4) = await (try fetchImage1, try fetchImage2, try fetchImage3, try fetchImage4)
+        return [image1, image2, image3, image4]
+    }
+    
+    private func fetchImage(urlString: String) async throws -> UIImage {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let image = UIImage(data: data) {
+                return image
+            } else {
+                throw URLError(.badURL)
+            }
+        } catch {
+            throw error
+        }
+    }
+}
+
+final class TaskGroupViewModel: ObservableObject {
     
     @Published var images: [UIImage] = []
+    let manager = TaskGroupDataManager()
+    
+    func getImages() async {
+        if let images = try? await manager.fetchImageWithyAsyncLet() {
+            self.images.append(contentsOf: images)
+        }
+    }
 }
 
 struct TaskGroup: View {
     
-    @StateObject private var viewModel = TaskgroupViewModel()
+    @StateObject private var viewModel = TaskGroupViewModel()
     let columns = [GridItem(.flexible()), GridItem(.flexible())]
     
     var body: some View {
@@ -30,6 +67,9 @@ struct TaskGroup: View {
                 }
             }
             .navigationTitle("Task Group")
+            .task {
+                await viewModel.getImages()
+            }
         }
     }
 }
