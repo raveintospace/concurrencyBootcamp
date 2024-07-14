ed239af84622908ee328acd23add1f333e16b702
Oriol Angelet <uriaeri@gmail.com>
2024-07-14 12:51:32 +0200

    L8, pt3 - Struct in detail

--- ConcurrencyBootcamp/ConcurrencyBootcamp/StructClassActor.swift
+++ ConcurrencyBootcamp/ConcurrencyBootcamp/StructClassActor.swift
@@ -38,9 +38,11 @@ extension StructClassActor {
     
     private func runTest() {
         print("Test started")
-        structTest1()
-        printDivider()
-        classTest1()
+//        structTest1()
+//        printDivider()
+//        classTest1()
+        
+        structTest2()
     }
     
     private func printDivider() {
@@ -79,5 +81,64 @@ extension StructClassActor {
         
         print("Object A: ", objectA.title)
         print("Object B: ", objectB.title)
+    }
+}
+
+// Immutable struct
+struct CustomStruct {
+    let title: String
+    
+    // creates a new object
+    func updateTitle(newTitle: String) -> CustomStruct {
+        return CustomStruct(title: newTitle)
+    }
+}
+
+// Mutable struct
+struct MutatingStruct {
+    
+    // private(set) so it can't be updated without using the func
+    // set allows us to access to property (ie print its value), but won't allow to be updated
+    private(set) var title: String
+    
+    // required as its property is private
+    init(title: String) {
+        self.title = title
+    }
+    
+    // updates the current object entirely
+    mutating func updateTitle(newTitle: String) {
+        title = newTitle
+    }
+}
+
+extension StructClassActor {
+    
+    private func structTest2() {
+        print("structTest2")
+        
+        print("Update value of mutable struct (var)")
+        var struct1 = MyStruct(title: "Title 1")
+        print("Struct1: ", struct1.title)
+        struct1.title = "Title 2"
+        print("Struct1: ", struct1.title)
+        
+        print("Update value of immutable struct (let)")
+        var struct2 = CustomStruct(title: "Title 1")
+        print("Struct2: ", struct2.title)
+        struct2 = CustomStruct(title: "Title2")
+        print("Struct2: ", struct2.title)
+        
+        print("Update value of immutable struct (let), second option")
+        var struct3 = CustomStruct(title: "Title 1")
+        print("Struct3: ", struct3.title)
+        struct3 = struct3.updateTitle(newTitle: "Title2")
+        print("Struct3: ", struct3.title)
+        
+        print("Update value of mutable struct")
+        var struct4 = MutatingStruct(title: "Title 1")
+        print("Struct4: ", struct4.title)
+        struct4.updateTitle(newTitle: "Title 2")
+        print("Struct4: ", struct4.title)
     }
 }
