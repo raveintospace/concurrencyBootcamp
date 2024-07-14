5e1f006cb38e85c79e10cbba3067688c34a3a817
Oriol Angelet <uriaeri@gmail.com>
2024-07-14 12:29:35 +0200

    L8, pt 2 - Class example

--- ConcurrencyBootcamp/ConcurrencyBootcamp/StructClassActor.swift
+++ ConcurrencyBootcamp/ConcurrencyBootcamp/StructClassActor.swift
@@ -20,8 +20,18 @@ struct StructClassActor: View {
     StructClassActor()
 }
 
+// Value type
 struct MyStruct {
-    let title: String
+    var title: String
+}
+
+// Reference type
+class MyClass {
+    var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
 }
 
 extension StructClassActor {
@@ -29,13 +39,45 @@ extension StructClassActor {
     private func runTest() {
         print("Test started")
         structTest1()
+        printDivider()
+        classTest1()
+    }
+    
+    private func printDivider() {
+        print("""
+        --------------------
+        """)
     }
     
     private func structTest1() {
+        print("structTest1")
         let objectA = MyStruct(title: "Starting title")
         print("Object A: ", objectA.title)
         
-        let objectB = objectA
+        print("Pass the VALUES of objectA to objectB")
+        var objectB = objectA       // creates a total new object
+        print("Object B: ", objectB.title)
+        
+        objectB.title = "Second title"
+        print("ObjectB title changed")      // changes title only for objectB
+        
+        print("Object A: ", objectA.title)
+        print("Object B: ", objectB.title)
+    }
+    
+    private func classTest1() {
+        print("classTest1")
+        let objectA = MyClass(title: "Starting title")
+        print("Object A: ", objectA.title)
+        
+        print("Pass the REFERENCE of objectA to objectB")
+        let objectB = objectA       // updates data of existing object
+        print("Object B: ", objectB.title)
+        
+        objectB.title = "Second title"      // updates title for every existing object (A & B)
+        print("ObjectB title changed")
+        
+        print("Object A: ", objectA.title)
         print("Object B: ", objectB.title)
     }
 }
