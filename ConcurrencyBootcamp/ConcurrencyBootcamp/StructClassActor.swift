aa7ddaa47ca8e1a14483e88417635078a33eb7eb
Oriol Angelet <uriaeri@gmail.com>
2024-07-15 19:13:38 +0200

    L8, final part - Actors and MVVM

--- ConcurrencyBootcamp/ConcurrencyBootcamp/StructClassActor.swift
+++ ConcurrencyBootcamp/ConcurrencyBootcamp/StructClassActor.swift
@@ -1,23 +1,114 @@
 //
 //  StructClassActor.swift
 //  ConcurrencyBootcamp
-//  https://youtu.be/-JLenSTKEcA?si=rEKfmKmex4gX1xnp, min 50 heap vs stack
+//  https://youtu.be/-JLenSTKEcA?si=rEKfmKmex4gX1xnp
 //  Created by Uri on 14/7/24.
 //
 
+// Lesson recap
+/*
+Value types:
+ - Struct, Enum, String, Int...
+ - Stored in the stack
+ - Faster
+ - Thread safe
+ - Creates a new copy of data
+ 
+ Reference types:
+ - Class, Functions, Actor
+ - Stored in the heap
+ - Slower but synchronized
+ - Not thread safe by default
+ - Creates a new reference/pointer to the original instance
+ 
+ -----------------------------
+ 
+ Stack:
+ - Stores value types
+ - Each thread has its own stack
+ 
+ Heap:
+ - Stores reference types
+ - Shared across threads
+ 
+ -----------------------------
+ 
+ Struct:
+ - Based on values
+ - Can be mutated
+ - Stored in the stack
+ 
+ Class:
+ - Based on references/instances
+ - Stored in the heap
+ - Can't be mutated outside the class, require a method in the class
+ - Can inherit from other classes, not common in SwiftUI
+ 
+ Actor:
+ - Same as class, but thread safe
+ 
+ -----------------------------
+ 
+ Struct: Data Models, because we want them to be fast and thread safe
+ Struct: Views
+ 
+ Class: ViewModels
+ 
+ Actor: For "shared" DataManagers and DataStores that are accessed by several locations in our app (ie, viewModels)
+ */
+
 import SwiftUI
 
+actor StructClassActorDataManager {
+    
+}
+
+// only initialized once
+class StructClassActorViewModel: ObservableObject {
+    
+    @Published var title: String = ""
+    
+    init() {
+        print("ViewModel INIT, view instantiates VM")
+    }
+}
+
+// initialized every time we update the view (ie updating isActive value)
 struct StructClassActor: View {
+    
+    @StateObject private var viewModel = StructClassActorViewModel()
+    let isActive: Bool
+    
+    init(isActive: Bool) {
+        self.isActive = isActive
+        print("View INIT")
+    }
+    
     var body: some View {
         Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea()
+            .background(isActive ? Color.red : Color.blue)
             .onAppear {
-                runTest()
+                //runTest()
+            }
+    }
+}
+
+struct StructClassActorHomeView: View {
+    
+    @State private var isActive: Bool = false
+    
+    var body: some View {
+        StructClassActor(isActive: isActive)
+            .onTapGesture {
+                isActive.toggle()
             }
     }
 }
 
 #Preview {
-    StructClassActor()
+    StructClassActor(isActive: true)
 }
 
 // MARK: - Struct Vs Class examples
@@ -47,6 +138,9 @@ extension StructClassActor {
         structTest2()
         printDivider()
         classTest2()
+        actorTest1()
+        printDivider()
+        classTest1()
     }
     
     private func printDivider() {
@@ -91,12 +185,12 @@ extension StructClassActor {
 // MARK: - Structs in depth
 
 // Immutable struct
-struct CustomStruct {
+struct ImmutableStruct {
     let title: String
     
     // creates a new object
-    func updateTitle(newTitle: String) -> CustomStruct {
-        return CustomStruct(title: newTitle)
+    func updateTitle(newTitle: String) -> ImmutableStruct {
+        return ImmutableStruct(title: newTitle)
     }
 }
 
@@ -130,13 +224,13 @@ extension StructClassActor {
         print("Struct1: ", struct1.title)
         
         print("Update value of immutable struct (let)")
-        var struct2 = CustomStruct(title: "Title 1")
+        var struct2 = ImmutableStruct(title: "Title 1")
         print("Struct2: ", struct2.title)
-        struct2 = CustomStruct(title: "Title2")
+        struct2 = ImmutableStruct(title: "Title2")
         print("Struct2: ", struct2.title)
         
         print("Update value of immutable struct (let), second option")
-        var struct3 = CustomStruct(title: "Title 1")
+        var struct3 = ImmutableStruct(title: "Title 1")
         print("Struct3: ", struct3.title)
         struct3 = struct3.updateTitle(newTitle: "Title2")
         print("Struct3: ", struct3.title)
@@ -150,9 +244,21 @@ extension StructClassActor {
 }
 
 
-// MARK: - Class in depth
+// MARK: - Class in depth + Actor
 
 class MyClass2 {
+    var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+    func updateTitle(newTitle: String) {
+        title = newTitle
+    }
+}
+
+actor MyActor {
     var title: String
     
     init(title: String) {
@@ -179,5 +285,23 @@ extension StructClassActor {
         print("Class 3: ", class3.title)
         class3.updateTitle(newTitle: "Title 2")
         print("Class 3: ", class3.title)
+    }
+    
+    private func actorTest1() {
+        Task {
+            print("actorTest1")
+            let objectA = MyActor(title: "Starting title")
+            await print("Object A: ", objectA.title)
+            
+            print("Pass the REFERENCE of objectA to objectB")
+            let objectB = objectA       // updates data of existing object
+            await print("Object B: ", objectB.title)
+            
+            await objectB.updateTitle(newTitle: "Second title") // updates title for every existing object (A & B)
+            print("ObjectB title changed")
+            
+            await print("Object A: ", objectA.title)
+            await print("Object B: ", objectB.title)
+        }
     }
 }
