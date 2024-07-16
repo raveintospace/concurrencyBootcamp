aa979c062b39b3ce334016d7734ccec3f2a183ee
Oriol Angelet <uriaeri@gmail.com>
2024-07-16 19:01:12 +0200

    L9, pt4 - Multithreading using actors

--- ConcurrencyBootcamp/ConcurrencyBootcamp/ActorExample.swift
+++ ConcurrencyBootcamp/ConcurrencyBootcamp/ActorExample.swift
@@ -8,8 +8,11 @@
 /*
  1. What is the problem that actors solve? When multiple threads try to access the same piece of memory in the heap at the same time
  
- 2. How to fix the problem before Actors? "private let queue" in Data Manager
+ 2. How to fix the problem before Actors / Make classes thread safe? "private let queue" in Data Manager
+ 
+ 3. Actors make classes thread safe by themselves
  */
+
 import SwiftUI
 
 class MyDataManager {
@@ -26,6 +29,21 @@ class MyDataManager {
             debugPrint(Thread())
             completionHandler(self.data.randomElement())
         }
+    }
+}
+
+actor MyActorDataManager {
+    
+    static let instance = MyActorDataManager()
+    private init() {}
+    
+    var data: [String] = []
+    
+    func getRandomData() -> String? {
+        self.data.append(UUID().uuidString)
+        debugPrint(Thread())
+        return data.randomElement()
+        
     }
 }
 
@@ -74,7 +92,7 @@ struct BrowseView: View {
         .onReceive(timer) { _ in
             DispatchQueue.global(qos: .default).async {
                 manager.getRandomData { title in
-                    if let daata = title {
+                    if let data = title {
                         DispatchQueue.main.async {
                             text = data
                         }
@@ -85,15 +103,67 @@ struct BrowseView: View {
     }
 }
 
+struct ActorHomeView: View {
+    
+    let manager = MyActorDataManager.instance
+    
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.green.opacity(0.3).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+            Task {
+                if let data = await manager.getRandomData() {
+                    await MainActor.run {
+                        text = data
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ActorBrowseView: View {
+    
+    let manager = MyActorDataManager.instance
+    
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.yellow.opacity(0.3).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+            Task {
+                if let daata = await manager.getRandomData() {
+                    await MainActor.run {
+                        text = data
+                    }
+                }
+            }
+        }
+    }
+}
+
 struct ActorExample: View {
     var body: some View {
         TabView {
-            HomeView()
+            ActorHomeView()
                 .tabItem {
                     Label("Home", systemImage: "house.fill")
                 }
             
-            BrowseView()
+            ActorBrowseView()
                 .tabItem {
                     Label("Browse", systemImage: "magnifyingglass")
                 }
