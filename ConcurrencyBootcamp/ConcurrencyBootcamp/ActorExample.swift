869bfa92cad25a53cd0c9a4cab97e6481d9c71c4
Oriol Angelet <uriaeri@gmail.com>
2024-07-16 18:43:13 +0200

    L9 pt2, checking that multiple threads acces the same piece of memory in the heap (using Thread Sanitizer)

--- ConcurrencyBootcamp/ConcurrencyBootcamp/ActorExample.swift
+++ ConcurrencyBootcamp/ConcurrencyBootcamp/ActorExample.swift
@@ -6,14 +6,73 @@
 //
 
 /*
- 1. What is the problem that actors solve? When two threads access the heap at the same time
+ 1. What is the problem that actors solve? When multiple threads try to access the same piece of memory in the heap at the same time
  */
 import SwiftUI
 
+class MyDataManager {
+    
+    static let instance = MyDataManager()
+    private init() {}
+    
+    var data: [String] = []
+    
+    func getRandomData() -> String? {
+        
+        self.data.append(UUID().uuidString)
+        debugPrint(Thread())
+        return data.randomElement()
+    }
+}
+
 struct HomeView: View {
+    
+    let manager = MyDataManager.instance
+    
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         ZStack {
             Color.green.opacity(0.3).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+            DispatchQueue.global(qos: .background).async {
+                if let data = manager.getRandomData() {
+                    DispatchQueue.main.async {
+                        text = data
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct BrowseView: View {
+    
+    let manager = MyDataManager.instance
+    
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.yellow.opacity(0.3).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+            DispatchQueue.global(qos: .default).async {
+                if let daaa = manager.getRandomData() {
+                    DispatchQueue.main.async {
+                        text = data
+                    }
+                }
+            }
         }
     }
 }
@@ -26,7 +85,7 @@ struct ActorExample: View {
                     Label("Home", systemImage: "house.fill")
                 }
             
-            Text("Hi 2")
+            BrowseView()
                 .tabItem {
                     Label("Browse", systemImage: "magnifyingglass")
                 }
