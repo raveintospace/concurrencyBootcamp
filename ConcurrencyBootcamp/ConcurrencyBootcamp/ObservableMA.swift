864898c773600904c2c2257813ae3ab5bf5ad4ce
Oriol Angelet <uriaeri@gmail.com>
2024-07-26 17:28:19 +0200

    L19, pt 1 - Observable example, using "old" ViewModel formula

--- ConcurrencyBootcamp/ConcurrencyBootcamp/ObservableMA.swift
+++ ConcurrencyBootcamp/ConcurrencyBootcamp/ObservableMA.swift
@@ -1,15 +1,44 @@
 //
 //  ObservableMA.swift
 //  ConcurrencyBootcamp
-//
+//  https://youtu.be/4dQOnNYjO58?si=L6chvoK7eAnGOgvj
 //  Created by Uri on 26/7/24.
 //
 
 import SwiftUI
+import Observation
+
+@Observable
+final class ObservableMAViewModel {
+    
+}
+
+actor TitleDataManager {
+    
+    func getNewTitle() -> String {
+        "Title from actor"
+    }
+}
+
+final class ObservableMAViewModelClassic: ObservableObject {
+    
+    @Published var title: String = "Starting title"
+    let manager = TitleDataManager()
+    
+    func updateTitle() async {
+        await title = manager.getNewTitle()
+    }
+}
 
 struct ObservableMA: View {
+    
+    @StateObject private var viewModel2 = ObservableMAViewModelClassic()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(viewModel2.title)
+            .task {
+                await viewModel2.updateTitle()
+            }
     }
 }
 
